b68b04f26c9ac684fe885e99cb3d96148aebc685
James Mitchell <mitchell@bitwise.io>
2017-02-22 22:48:56 -0700

    Adds initial batch transaction dependency checking to completer
    Signed-off-by: James Mitchell <mitchell@bitwise.io>

--- validator/sawtooth_validator/journal/completer.py
+++ validator/sawtooth_validator/journal/completer.py
@@ -21,6 +21,7 @@ from sawtooth_validator.journal.timed_cache import TimedCache
 from sawtooth_validator.protobuf.batch_pb2 import Batch
 from sawtooth_validator.protobuf.batch_pb2 import BatchList
 from sawtooth_validator.protobuf.block_pb2 import Block
+from sawtooth_validator.protobuf.transaction_pb2 import TransactionHeader
 from sawtooth_validator.protobuf import client_pb2
 from sawtooth_validator.protobuf import network_pb2
 from sawtooth_validator.protobuf import validator_pb2
@@ -35,6 +36,13 @@ class Completer(object):
     def __init__(self, block_store):
         self.batch_cache = TimedCache()
         self.block_cache = BlockCache(block_store)
+        # The _seen_txns list is a mechanism for tracking which
+        # transactions appear in complete batches. This should be
+        # replaced with a different mechanism which can query
+        # transaction information from the current chain and the
+        # pending batch list.
+        self._seen_txns = []
+        self._incomplete_batches = []
         self._on_block_received = None
         self._on_batch_received = None
 
@@ -56,9 +64,29 @@ class Completer(object):
                 LOGGER.debug("Block discarded(Missing batch): %s",
                              block.header_signature[:8])
                 return False
-
-        self.block_cache[block.header_signature] = block
         return True
+
+    def _check_batch(self, batch):
+        for txn in batch.transactions:
+            txn_header = TransactionHeader()
+            txn_header.ParseFromString(txn.header)
+            for dependency in txn_header.dependencies:
+                if dependency not in self._seen_txns:
+                    LOGGER.debug("Transaction %s in batch %s has "
+                                 "unsatisfied dependency: %s",
+                                 txn.header_signature,
+                                 batch.header_signature,
+                                 dependency)
+                    return False
+        return True
+
+    def _add_seen_txns(self, batch):
+        for txn in batch.transactions:
+            self._seen_txns.append(txn.header_signature)
+
+    def _process_incomplete_batches(self):
+        for batch in self._incomplete_batches:
+            self.add_batch(batch)
 
     def set_on_block_received(self, on_block_received_func):
         self._on_block_received = on_block_received_func
@@ -69,11 +97,20 @@ class Completer(object):
     def add_block(self, block):
         blkw = BlockWrapper(block)
         if self._check_block(blkw):
+            self.block_cache[block.header_signature] = blkw
             self._on_block_received(blkw)
 
     def add_batch(self, batch):
-        self.batch_cache[batch.header_signature] = batch
-        self._on_batch_received(batch)
+        if self._check_batch(batch):
+            self._add_seen_txns(batch)
+            self.batch_cache[batch.header_signature] = batch
+            self._on_batch_received(batch)
+            if batch in self._incomplete_batches:
+                self._incomplete_batches.remove(batch)
+            self._process_incomplete_batches()
+        else:
+            if batch not in self._incomplete_batches:
+                self._incomplete_batches.append(batch)
 
 
 class CompleterBatchListBroadcastHandler(Handler):
